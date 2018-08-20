ca7e9f755b970d9438526172670b924fe5058801
DESKTOP-E3LAKDE\joshp <joshpulejo@gmail.com>
2018-08-20 16:02:09 -0500

    Lec 142, Logged out Barrel Rotation, and AIm At Rotation, and the DELTA  of the 2 Rotations.

--- BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
@@ -20,26 +20,6 @@ void UTankAimingComponent::SetBarrellReference(UStaticMeshComponent* BarrelToSet
 }
 
 
-
-
-// Called when the game starts
-void UTankAimingComponent::BeginPlay()
-{
-	Super::BeginPlay();
-
-	// ...
-	
-}
-
-
-// Called every frame
-void UTankAimingComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
-{
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-
-	// ...
-}
-
 void UTankAimingComponent::AimAt(FVector OutHitLocation, float LaunchSpeed)
 {
 	/*
@@ -53,28 +33,46 @@ void UTankAimingComponent::AimAt(FVector OutHitLocation, float LaunchSpeed)
 		FVector OutLaunchVelocity;   // Initialized to  Zero(0).
 		FVector StartLocation = Barrel->GetSocketLocation(FName("Projectile")); // If String isnt found it returns to the "Barrel". fails nicely. 
 	
-	// Returns a bool
-	if (UGameplayStatics::SuggestProjectileVelocity 
+
+	bool bHaveAimSolution = UGameplayStatics::SuggestProjectileVelocity 
 	(
 		this,
 		OutLaunchVelocity,
 		StartLocation,
 		OutHitLocation,
 		LaunchSpeed,
-		false,
-		0,
-		0,
 		ESuggestProjVelocityTraceOption::DoNotTrace
 
-	)
-		)    // Calculate The OutLaunchVelocity, then:
-	
+	); 
+	// Calculate The OutLaunchVelocity, then:
+		if (bHaveAimSolution)
 	{
 		auto AimDirection = OutLaunchVelocity.GetSafeNormal(); // GetSafeNormal() turns it into a Unit Vector.
-		auto TankName = GetOwner()->GetName();
-		UE_LOG(LogTemp, Warning, TEXT(" %s Firing At:  %s "), *TankName, *AimDirection.ToString());
-		// You Only use a  '*'  if you're trying to get an FString out of the Variable, in this case the FString is our name.
+		MoveBarrelTowards(AimDirection);
+		}
 
-	}
 }
 
+void UTankAimingComponent::MoveBarrelTowards(FVector AimDirection)
+{
+	///Bens
+	//*Mine 
+	
+		/// Work-out current difference between Barrel Rotation, and Aim Direction
+		//	*Assign Our Input (I.E. our Mouse/Gamepad Movement to the CrossHair, and make the Barrell Follow The CrossHair/Input
+
+	auto BarrelRotator = Barrel->GetForwardVector().Rotation(); // Gets the Rotation at this moment it's pretty Static, no need to log out. Lec 142
+	auto AimAsRotator = AimDirection.Rotation();
+	auto DeltaRotator = AimAsRotator - BarrelRotator;
+
+	UE_LOG(LogTemp, Warning, TEXT(" DeltaRotator: %s "), *DeltaRotator.ToString());
+
+		/// Move the Barrel the right amount this frame
+		//	*Allow for A limited Degree Of Motion, I.E. 360 Degrees, or 180, How Far Up Can it go How Far Down
+		
+		///Given a Max Elevation Speed and the Frame Time.
+		//	*returns OutHitLocation but with the added benefit of moving the Barrel.
+
+		
+
+}
--- BattleTank/Source/BattleTank/Public/TankAimingComponent.h
+++ BattleTank/Source/BattleTank/Public/TankAimingComponent.h
@@ -24,26 +24,15 @@ public:
 
 	void SetBarrellReference(UStaticMeshComponent* BarrelToSet);
 
-
+	void MoveBarrel();
 
 protected:
-
-
-	// Called when the game starts
-	virtual void BeginPlay() override;
-
-
-public:	
-
-
-	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
-
 	
 
 private:
 	
 	UStaticMeshComponent* Barrel = nullptr;
 
+	void MoveBarrelTowards(FVector AimDirection);
 	
 };
